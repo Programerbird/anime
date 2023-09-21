f7d1a6297f1c7784e4b1223d349aad4a83deb718
Programerbird <programerbird@gmail.com>
2023-09-21 14:13:11 -0700

    anime-version2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Header.css';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange} from '@mui/material/colors';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import AbcIcon from '@mui/icons-material/Abc';
+import AccessibleIcon from '@mui/icons-material/Accessible';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className='wrapper_header' >
+        <div className='header_main_box' >
+        <Avatar sx={{ bgcolor: deepOrange[500] }} variant="square">
+          A
+        </Avatar> 
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
