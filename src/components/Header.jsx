cb812c65bec31ce6e12278529c4958bf6b7720e2
Programerbird <programerbird@gmail.com>
2023-09-21 19:01:51 -0700

    from_pc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css';
 import Avatar from '@mui/material/Avatar';
-import { deepOrange} from '@mui/material/colors';
+import { deepOrange,green} from '@mui/material/colors';
 import AvatarGroup from '@mui/material/AvatarGroup';
 import AbcIcon from '@mui/icons-material/Abc';
 import AccessibleIcon from '@mui/icons-material/Accessible';
@@ -11,7 +11,7 @@ const Header = () => {
     <div>
       <div className='wrapper_header' >
         <div className='header_main_box' >
-        <Avatar sx={{ bgcolor: deepOrange[500] }} variant="square">
+        <Avatar sx={{ bgcolor: green[500] }} variant="square">
           A
         </Avatar> 
           
