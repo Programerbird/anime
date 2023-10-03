a2b61df674720bdcca2ee79c1186425a336a1f39
Programerbird <programerbird@gmail.com>
2023-10-02 18:22:09 -0700

    upate_10-2-23

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,22 +10,16 @@ import mainlogo from '../Pictures/mainlogo.png'
 const Header = () => {
   return (
     <div className='head_wrapper' >
-      <div className='main' >
-        <div className='mainlogo' >
-          <img className='logo' src={mainlogo} alt='mainlogopic'/>
-            <div className='mainbutton' >
-            <div className='button' >
-            <Button  variant="contained" color="success" >login
-            </Button>
-            <Button variant="contained" color="success" className='button__name'>Sign up
-            </Button>
-            </div>
-            </div>
-          
-         
-            
+      <div className='logo'>
+        <img     className='anime' src={mainlogo} />
+          <div className='my_button' >
+        <Button  variant="contained" color="success" >login
+        </Button>
+        <Button  variant="contained" color="success" >login
+        </Button>
         </div>
       </div>
+
     </div>
     
      
