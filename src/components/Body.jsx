6607f8836ee503983aa7ff8ee3afbd216f2cf352
Water Palma <programerbird@gmail.com>
2023-10-14 13:21:52 -0700

    update_1%

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Body.css';
+import mainlogo from '../Pictures/mainlogo.png'
 import pic1 from '../Pictures/pic1.jpg'
 import pic2 from '../Pictures/pic2.jpg'
 import pic3 from '../Pictures/pic3.jpg'
@@ -16,20 +17,21 @@ function Body() {
   return (
     <div>
       <div className='main_wrapper' >
+        <img className='bk_pic' src={mainlogo}  img/>
         <h1>Login</h1>
           <div className='placeholders' >
-            <input type='text' placeholder='Email'input/>
-            <input type='text' placeholder='Password'input/>
+            <input  type='text' placeholder='Email'input/>
+            <input type='password' placeholder='Password'input/>
           </div>
             <div className='buttons' >
               <Button variant="contained" color="success">
                 Login
               </Button>
-                <a href="url">Forgot Password? </a>
+                <a className='link_1' href="url">Forgot Password? </a>
               </div>
-                <div>
-                 <h4>Dont have an account?</h4>
-                 <a href='url'>Sign up</a>
+                <div className='sign_up' >
+                 <h4>Don't have an account?</h4>
+                 <a className='link_2'  href='url'>Sign up</a>
                 </div>
       </div>
     </div>
